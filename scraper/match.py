5fcbc1f0466f16f11919bba57926d552aca7627c
Yuhan Liu <yliu1021@outlook.com>
2022-06-14 02:16:24 -0700

    scraper shuffles matches for more diverse scraping

--- scraper/match.py
+++ scraper/match.py
@@ -4,6 +4,7 @@ Scrapes a bunch of matches and stores them
 import asyncio
 import sys
 import os
+import random
 
 from motor.motor_asyncio import AsyncIOMotorClient
 from pymongo import errors
@@ -41,8 +42,9 @@ async def main(first_summoner: str):
             if puuid in seen_puuids:
                 continue
             seen_puuids.add(puuid)
-            matchlist = await lc.match.match_history(puuid=puuid, start_time=1654758000, type="MATCHED_GAME", count=100)
+            matchlist = await lc.match.match_history(puuid=puuid, start_time=1654758000, count=100)
             matchlist = [m for m in matchlist if m not in seen_matches]
+            random.shuffle(matchlist)
             should_retry = True
             while should_retry:
                 should_retry = False
